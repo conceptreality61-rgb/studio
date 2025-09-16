7e8c5bc55f002a9f22d53fa5e4751425dd1f7c49
concept reality <conceptreality61@gmail.com>
2025-09-16 18:43:33 +0000

    increase font size with white colour (_for element p_)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
           <h1 className="text-4xl md:text-6xl font-headline font-bold drop-shadow-lg text-white">
             Your Home, Spotlessly Clean
           </h1>
-          <p className="mt-4 max-w-2xl text-lg md:text-xl drop-shadow-md text-primary font-bold">
+          <p className="mt-4 max-w-2xl text-xl md:text-2xl drop-shadow-md text-white font-bold">
             Book trusted and professional household services in just a few clicks.
             From cleaning to gardening, we've got you covered.
           </p>
