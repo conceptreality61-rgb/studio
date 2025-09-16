d28052be6d024ac42b1a666d7f9c14a446cbd574
concept reality <conceptreality61@gmail.com>
2025-09-16 18:28:21 +0000

    it is in developing mode now so message not getting on actual mail id

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -37,7 +37,7 @@ type AuthFormProps = {
 
 type Role = 'customer' | 'worker' | 'manager';
 
-const SUPERADMIN_EMAIL = 'conceptreality61@gmail.com';
+const SUPERADMIN_EMAIL = 'manager@cleansweep.com';
 
 function AuthFormFields({ isSignUp, role, onAuthSuccess }: { isSignUp?: boolean; role: Role; onAuthSuccess: (role: Role) => void; }) {
   const { toast } = useToast();
